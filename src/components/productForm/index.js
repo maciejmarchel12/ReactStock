ec0886e2f5fa74e008d65f9f9e6d3cab6a1add98
Maciej Marchel <maciejmarchel14@gmail.com>
2024-02-19 21:16:38 +0000

    Fixed Product Page Format

--- src/components/productForm/index.js
+++ src/components/productForm/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
 
 const ProductForm = ({ onAddProduct }) => {
   console.log('onAddProduct:', onAddProduct);
@@ -37,54 +38,73 @@ const ProductForm = ({ onAddProduct }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <TextField
-        label="Product Name"
-        name="productName"
-        value={formData.productName}
-        onChange={handleChange}
-        required
-      />
-      <TextField
-        label="Image URL"
-        name="image"
-        value={formData.image}
-        onChange={handleChange}
-        required
-      />
-      <TextField
-        label="Product Type"
-        name="productType"
-        value={formData.productType}
-        onChange={handleChange}
-        required
-      />
-      <TextField
-        label="Barcode"
-        name="barcode"
-        value={formData.barcode}
-        onChange={handleChange}
-        required
-      />
-      <TextField
-        label="Amount Available"
-        name="amountAvailable"
-        type="number"
-        value={formData.amountAvailable}
-        onChange={handleChange}
-        required
-      />
-      <TextField
-        label="Store Location"
-        name="storeLocation"
-        value={formData.storeLocation}
-        onChange={handleChange}
-        required
-      />
-      <Button type="submit" variant="contained" color="primary">
-        Add Product
-      </Button>
-    </form>
+    <Box
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      mt={5}
+    >
+      <form onSubmit={handleSubmit}>
+        <Box mb={2}>
+          <TextField
+            label="Product Name"
+            name="productName"
+            value={formData.productName}
+            onChange={handleChange}
+            required
+          />
+        </Box>
+        <Box mb={2}>
+          <TextField
+            label="Image URL"
+            name="image"
+            value={formData.image}
+            onChange={handleChange}
+            required
+          />
+        </Box>
+        <Box mb={2}>
+          <TextField
+            label="Product Type"
+            name="productType"
+            value={formData.productType}
+            onChange={handleChange}
+            required
+          />
+        </Box>
+        <Box mb={2}>
+          <TextField
+            label="Barcode"
+            name="barcode"
+            value={formData.barcode}
+            onChange={handleChange}
+            required
+          />
+        </Box>
+        <Box mb={2}>
+          <TextField
+            label="Amount Available"
+            name="amountAvailable"
+            type="number"
+            value={formData.amountAvailable}
+            onChange={handleChange}
+            required
+          />
+        </Box>
+        <Box mb={2}>
+          <TextField
+            label="Store Location"
+            name="storeLocation"
+            value={formData.storeLocation}
+            onChange={handleChange}
+            required
+          />
+        </Box>
+        <Button type="submit" variant="contained" color="primary">
+          Add Product
+        </Button>
+      </form>
+    </Box>
   );
 };
 
