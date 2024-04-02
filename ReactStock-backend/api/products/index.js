99dd6b961899a0ea7be2629264453343c4ed5a59
Maciej Marchel <maciejmarchel14@gmail.com>
2024-04-02 16:41:06 +0100

    Barcode Scanning added

--- ReactStock-backend/api/products/index.js
+++ ReactStock-backend/api/products/index.js
@@ -67,4 +67,70 @@ router.delete('/:id', async (req, res) => {
     }
 });
 
+//Barcode Scanning
+
+// Route to get a product by barcode
+router.get('/barcode/:barcode', async (req, res) => {
+  try {
+      const product = await Product.findOne({ barcode: req.params.barcode });
+      if (!product) {
+          return res.status(404).json({ message: 'Product not found' });
+      }
+      res.status(200).json(product);
+  } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+// Route to handle barcode scanning and update product amount
+router.post('/scan', async (req, res) => {
+  try {
+    const { barcode, isSubtractMode } = req.body;
+    const product = await Product.findOne({ barcode });
+
+    if (!product) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
+
+    // Update amount available based on subtract mode
+    if (isSubtractMode) {
+      product.amountAvailable = Math.max(0, product.amountAvailable - 1); // Ensure amount doesn't go negative
+    } else {
+      product.amountAvailable++;
+    }
+
+    await product.save();
+
+    res.status(200).json(product);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
+// Route to update a product by barcode
+router.put('/updateByBarcode/:barcode', async (req, res) => {
+  try {
+      const { barcode } = req.params;
+      const updatedData = req.body;
+      const product = await Product.findOne({ barcode });
+
+      if (!product) {
+          return res.status(404).json({ message: 'Product not found' });
+      }
+
+      const updatedProduct = await Product.findByIdAndUpdate(product._id, updatedData, { new: true });
+
+      if (!updatedProduct) {
+          return res.status(404).json({ message: 'Error updating product' });
+      }
+
+      res.status(200).json(updatedProduct);
+  } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Internal server error' });
+  }
+});
+
 export default router;
