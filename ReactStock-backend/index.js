2dc63121d2001b133080d8b2516e6c3d06cd23f2
Maciej Marchel <maciejmarchel14@gmail.com>
2024-04-29 18:58:46 +0100

    Hosting on Render Port added

--- ReactStock-backend/index.js
+++ ReactStock-backend/index.js
@@ -12,7 +12,7 @@ dotenv.config();
 
 const app = express();
 //const port = process.env.PORT || 8080;
-const port = process.env.PORT; 
+const port = process.env.PORT || 3000; 
 
 app.use(cors())
 app.use(express.json());
